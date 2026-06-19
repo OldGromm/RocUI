f3a0f2475710bdfc78727a46ce9824d09161e0fb
OldGromm <oldgromm@protonmail.com>
2026-06-19 10:50:23 +0200

    1.03 Update
    -Updated TOC version.
    -Fixed custom sound effects overlapping when playing in quick succession.

--- RoCUI_SoundOverrides.lua
+++ RoCUI_SoundOverrides.lua
@@ -1,7 +1,9 @@
-RoCUI_CustomSound_QuestAccepted = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestNew.ogg"
-RoCUI_CustomSound_QuestCompleted = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestCompleted.ogg"
-RoCUI_CustomSound_PlayerDies = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestFailed.ogg"
-RoCUI_CustomSound_Queue = "Interface\\AddOns\\RoCUI\\sounds\\interface\\GameFound.ogg"
+RoCUI_CustomSound = {}
+RoCUI_CustomSound["QUEST_ACCEPTED"] = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestNew.ogg"
+RoCUI_CustomSound["QUEST_TURNED_IN"] = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestCompleted.ogg"
+RoCUI_CustomSound["PLAYER_DEAD"] = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestFailed.ogg"
+RoCUI_CustomSound["LFG_PROPOSAL_SHOW"] = "Interface\\AddOns\\RoCUI\\sounds\\interface\\GameFound.ogg"
+RoCUI_SoundHandle = {}
 
 
 
@@ -9,34 +11,55 @@ RoCUI_CustomSound_Queue = "Interface\\AddOns\\RoCUI\\sounds\\interface\\GameFoun
 function RoCUI_SoundReplacements(input_settingsvariable, input_toggle)
 	if input_settingsvariable == "RoCUI_Options_SoundOverride_QuestAccepted" then
 	    if input_toggle == true then
-		    RoCUI_CustomSound_QuestAccepted = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestNew.ogg"
+		    RoCUI_CustomSound["QUEST_ACCEPTED"] = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestNew.ogg"
 			MuteSoundFile(567400)
 		else
-		    RoCUI_CustomSound_QuestAccepted = ""
+		    RoCUI_CustomSound["QUEST_ACCEPTED"] = ""
 			UnmuteSoundFile(567400)
 		end
 	elseif input_settingsvariable == "RoCUI_Options_SoundOverride_QuestCompleted" then
 	    if input_toggle == true then
-		    RoCUI_CustomSound_QuestCompleted = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestCompleted.ogg"
+		    RoCUI_CustomSound["QUEST_TURNED_IN"] = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestCompleted.ogg"
 			MuteSoundFile(567439)
 		else
-		    RoCUI_CustomSound_QuestCompleted = ""
+		    RoCUI_CustomSound["QUEST_TURNED_IN"] = ""
 			UnmuteSoundFile(567439)
 		end
 	elseif input_settingsvariable == "RoCUI_Options_SoundOverride_PlayerDies" then
 	    if input_toggle == true then
-		    RoCUI_CustomSound_PlayerDies = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestFailed.ogg"
+		    RoCUI_CustomSound["PLAYER_DEAD"] = "Interface\\AddOns\\RoCUI\\sounds\\quests\\QuestFailed.ogg"
 		else
-		    RoCUI_CustomSound_PlayerDies = ""
+		    RoCUI_CustomSound["PLAYER_DEAD"] = ""
 		end
 	elseif input_settingsvariable == "RoCUI_Options_SoundOverride_Queue" then
 	    if input_toggle == true then
-		    RoCUI_CustomSound_Queue = "Interface\\AddOns\\RoCUI\\sounds\\interface\\GameFound.ogg"
+		    RoCUI_CustomSound["LFG_PROPOSAL_SHOW"] = "Interface\\AddOns\\RoCUI\\sounds\\interface\\GameFound.ogg"
 			MuteSoundFile(567478)
 		else
-		    RoCUI_CustomSound_Queue = ""
+		    RoCUI_CustomSound["LFG_PROPOSAL_SHOW"] = ""
 			UnmuteSoundFile(567478)
 		end
 	else
+	end
+end
+
+
+
+
+function RoCUI_SoundPlayer(soundtype)
+    local RoCUI_Temp_SoundHandle = RoCUI_SoundHandle[soundtype]
+	local RoCUI_Temp_SoundID = RoCUI_CustomSound[soundtype]
+	local RoCUI_Temp_Proceed = false
+	
+	if RoCUI_Temp_SoundHandle == nil then
+	    RoCUI_Temp_Proceed = true
+	else
+		if C_Sound.IsPlaying(RoCUI_Temp_SoundHandle) == false then
+            RoCUI_Temp_Proceed = true    
+        end
+	end
+
+    if RoCUI_Temp_Proceed == true then
+	    _, RoCUI_SoundHandle[soundtype] = PlaySoundFile(RoCUI_Temp_SoundID, "SFX")
 	end
 end
